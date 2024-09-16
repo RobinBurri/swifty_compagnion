eb21497b99869e1f7160c54c9eaf5f559cf87c88
Robin Burri <robinburri@tutanota.com>
2024-09-16 18:36:06 +0200

    add colors

---
+++ constants/styles.ts
@@ -0,0 +1,12 @@
+export const GlobalStyles = {
+    colors: {
+      red: '#da2c38',
+      darkGreen: '#226f54',
+      lightGreen: '#87c38f',
+      beige: '#f4f0bb',
+      darkBrown: '#43291f',
+      black: '#000000',
+      light: '#f6fff8',
+      white: '#ffffff',
+    },
+  };
