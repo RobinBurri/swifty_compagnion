ce5400ee7d6fe698cfd27bebe6184f2a03e14deb
Robin Burri <robinburri@tutanota.com>
2024-09-17 12:50:18 +0200

    change name file

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import AppNavigationStack from './AppNavigationStack'
+import { AuthContextProvider } from './store/auth-context'
 
 export default function App() {
     return (
         <>
             <StatusBar style="auto" />
             <NavigationContainer>
-                <AppNavigationStack />
+                <AuthContextProvider>
+                    <AppNavigationStack />
+                </AuthContextProvider>
             </NavigationContainer>
         </>
     )
