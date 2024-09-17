24d2e0bb8713a0fe241dba36592579eebd0d7a18
Robin Burri <robinburri@tutanota.com>
2024-09-17 16:40:08 +0200

    creating fullStudent

--- utils/getStudent.tsx
+++ utils/getStudent.tsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useCallback, useContext } from 'react'
 import { API_URL } from '../constants/apiUrl'
 import BasicStudent from '../models/BasicStudent'
+import FullStudent from '../models/FullStudent'
 import { AuthContext } from '../store/auth-context'
 
 const ALL_USERS = '/v2/users'
@@ -41,33 +42,79 @@ export const useStudentList = () => {
 }
 
 export const useStudentById = () => {
-    const authCtx = useContext(AuthContext);
-  
-    const getStudentById = useCallback(async (studentLogin: string) => {
-      if (!authCtx) {
-        console.error('Auth context is not available');
-        return;
-      }
-  
-      try {
-        const token = await authCtx.getToken();
-        const response = await axios.get(
-          `${API_URL}${ALL_USERS}/${studentLogin}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-  
-        const student = response.data;
-        console.log(student);
-        return student;
-      } catch (error) {
-        console.error('Failed to get student:', error);
-        return undefined;
-      }
-    }, [authCtx]);
-  
-    return { getStudentById };
-  };
+    const authCtx = useContext(AuthContext)
+
+    const getStudentById = useCallback(
+        async (studentLogin: string) => {
+            if (!authCtx) {
+                console.error('Auth context is not available')
+                return
+            }
+
+            try {
+                const token = await authCtx.getToken()
+                const response = await axios.get(
+                    `${API_URL}${ALL_USERS}/${studentLogin}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                )
+
+                const student = createStudent(response.data)
+                console.log(student)
+                return student
+            } catch (error) {
+                console.error('Failed to get student:', error)
+                return undefined
+            }
+        },
+        [authCtx]
+    )
+
+    return { getStudentById }
+}
+
+const createStudent = (studentData: any): FullStudent => {
+    console.log(studentData)
+
+    console.log("-------------------")
+    console.log(studentData.campus[0].city)
+    console.log(studentData.campus[0].country)
+    console.log(studentData.first_name)
+    console.log(studentData.last_name)
+    console.log(studentData.email)
+    console.log(studentData.image.link)
+    console.log(studentData.level)
+    console.log(studentData.grade)
+    console.log(studentData.projects)
+    console.log(studentData.correction_point)
+    console.log("-------------------")
+
+
+    const newStudent = new FullStudent(
+        studentData.campus[0].city,
+        studentData.campus[0].country,
+        studentData.first_name,
+        studentData.last_name,
+        studentData.email,
+        studentData.image.link,
+        studentData.level,
+        studentData.grade,
+        studentData.projects,
+        studentData.correction_point
+    )
+    return newStudent
+}
+
+// private city: string,
+// private country: string,
+// private firstName: string,
+// private lastName: string,
+// private email: string,
+// private image: string,
+// private level: number,
+// private grade: string,
+// private projects: project[],
+// private correctionPoints: number
