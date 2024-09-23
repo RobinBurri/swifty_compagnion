de585e64e368e98ba46d48c41895d4d82c3ffbab
Robin Burri <robinburri@tutanota.com>
2024-09-23 09:27:21 +0200

    merged app.json and app.config.json

--- app.config.js
+++ app.config.js
@@ -2,9 +2,38 @@ import 'dotenv/config';
 
 export default {
   expo: {
+    name: "swifty_companion",
+    slug: "swifty_companion",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/42_logo.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      buildNumber: "1.0.0",
+      supportsTablet: true,
+      bundleIdentifier: "com.42.swifty_companion"
+    },
+    android: {
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
     extra: {
       UID: process.env.UID,
       SECRET: process.env.SECRET,
-    },
-  },
+      eas: {
+        projectId: "5634d872-aa8a-4495-8cea-c8228203431f"
+      }
+    }
+  }
 };
